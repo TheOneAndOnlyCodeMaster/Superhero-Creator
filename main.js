59f2faa855167c9918db94dc291557823f7a56e6
TheOneAndOnlyCodeMaster <74931617+TheOneAndOnlyCodeMaster@users.noreply.github.com>
2021-03-25 20:50:10 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,52 @@
 canvas = new fabric.Canvas('canvas');
 
 object_x = 10;
-object_y = 10;
+object_y = 100;
 
 object_width = 140
 object_height = 150
 
+player_x = 10;
+player_y = 10;
+
+player_width = 100
+player_height = 110
+
+player_object = "baby_groot.png";
+player_object_real = "";
+
 var body_part = "";
 
+function load_player(player_object){
+    fabric.Image.fromURL(player_object, function(player_object){
+        player_object_real = player_object;
+        player_object_real.scaleToWidth(player_width);
+        player_object_real.scaleToHeight(player_height);    
+        player_object_real.set({
+            top:player_y,
+            left:player_x
+        });
+    canvas.add(player_object_real);
+})
+}
+load_player();
 function load_body_part(get_image){
         fabric.Image.fromURL(get_image, function(img){
             body_part = img;
             body_part.scaleToWidth(object_width);
             body_part.scaleToHeight(object_height);    
             body_part.set({
-                top:player_pos_y,
-                left:player_pos_x
+                top:object_y,
+                left:object_x
             });
-        canvas.add(block_object);
+        canvas.add(body_part);
     })
 }
 
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e){
-keyPress = e.keycode
+keyPress = e.keyCode
 console.log(keyPress);
 
     if(e.shiftKey && keyPress == '77'){
@@ -61,4 +83,28 @@ console.log(keyPress);
         console.log('l');
         load_body_part("captain_america_left_hand.png");
     }
+    if(keyPress == 38){
+        up();
+    }
+    if(keyPress == 40){
+        down();
+    }
+    if (keyPress == 39){
+        right();
+    }
+    if(keyPress == 37){
+        left();
+    }
+}
+function up(){
+
+}
+function down(){
+
+}
+function right(){
+
+}
+function left(){
+
 }
